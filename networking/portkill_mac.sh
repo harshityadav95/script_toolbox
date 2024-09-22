937f8f93264a0abcea61d3b096afdc718bf943c2
Harshit Yadav <harshityadav@outlook.com>
2024-09-22 12:54:51 +0530

    Update portkill_mac.sh

--- networking/portkill_mac.sh
+++ networking/portkill_mac.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Check if a process is running on port 8081
+PID=$(lsof -ti:8501)
+
+if [ -n "$PID" ]; then
+  echo "Process running on port 8081 with PID: $PID"
+  echo "Killing process..."
+  kill -9 $PID
+  echo "Process killed."
+else
+  echo "No process running on port 8081."
+fi
